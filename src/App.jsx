7416e8448f948d3c8ba94c13aab20ee9535e4ee0
Mailan Franco <36058994+thesckurtt@users.noreply.github.com>
2025-05-12 10:55:13 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,17 @@ import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
 import Home from './pages/Home'
 import Login from './pages/login'
 import NotFound from './pages/NotFound'
+import Game from './pages/game'
+import Register from './pages/register'
+
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/game' element={<Game />} />
         <Route path='*' element={<NotFound />} />
       </Routes>
     </Router>
