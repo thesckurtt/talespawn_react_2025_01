70a9cc8233abb8ab491974c35146f38c1ecb295e
Mailan Franco <36058994+thesckurtt@users.noreply.github.com>
2025-05-12 10:35:45 -0300

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
+import Home from './pages/Home'
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <Router>
+      <Route path='/' element={<Home />}/>
+    </Router>
   )
 }
 
